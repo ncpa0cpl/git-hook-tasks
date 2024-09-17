49fb31ecd6c41c3cb792184bc7d254fc987c9eb8
ncpa0cpl <szymonb21@gmail.com>
2024-09-17 17:55:33 +0200

    add run after configure

--- src/bin/bin.ts
+++ src/bin/bin.ts
@@ -6,10 +6,18 @@ import { PreCommitCommand } from "../commands/precommit";
 import { PrePushCommand } from "../commands/prepush";
 import { OutputManager } from "../utilities/output/output-manager";
 
-clify.configure((main) => {
-  OutputManager.setMaxFps(2.86);
-  main.command("install", InstallCommand);
-  main.command("pre-commit", PreCommitCommand);
-  main.command("pre-push", PrePushCommand);
-  main.command("post-commit", PostCommitCommand);
-});
+clify
+  .configure((main) => {
+    OutputManager.setMaxFps(2.86);
+
+    main.setName("git-hook-tasks");
+    main.setDescription(
+      "A runner for git hooks with support for parallel tasks."
+    );
+
+    main.command("install", InstallCommand);
+    main.command("pre-commit", PreCommitCommand);
+    main.command("pre-push", PrePushCommand);
+    main.command("post-commit", PostCommitCommand);
+  })
+  .run();
