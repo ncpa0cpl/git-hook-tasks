8e941f8a0104499cd292c6b9527e321a5d538847
ncpa0cpl <szymonb21@gmail.com>
2024-09-17 17:59:57 +0200

    fix findProjectRoot fn

--- src/utilities/find-project-root.ts
+++ src/utilities/find-project-root.ts
@@ -5,7 +5,7 @@ import path from "path";
 const isWindows = os.platform() === "win32";
 
 export const findProjectRoot = async () => {
-  let location = path.resolve(__dirname, "../../..");
+  let location = path.resolve(__dirname, "..");
 
   while (true) {
     if ((isWindows && location.length < 4) || location.length < 2) {
@@ -15,7 +15,14 @@ export const findProjectRoot = async () => {
     const files = await fs.readdir(location);
 
     if (files.some((f) => f === "package.json")) {
-      return location;
+      const fpath = path.resolve(location, "package.json");
+      const packagejson = await fs
+        .readFile(fpath, "utf8")
+        .then((f) => JSON.parse(f));
+
+      if (packagejson.name !== "git-hook-tasks") {
+        return location;
+      }
     }
 
     location = path.resolve(location, "..");
